008aa19a8a03204b2e9562b42aa5588ba26c7172
Niamh Power <niamhpower@live.com>
2023-02-07 20:42:17 +0000

    project setup

---
+++ Kickr/Shared/KickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  KickrApp.swift
+//  Shared
+//
+//  Created by Niamh Power on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct KickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
