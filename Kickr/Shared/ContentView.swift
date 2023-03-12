902a7ac7f179d75deebb1615ba61ee1e7422dba9
Niamh Power <niamh-power@users.noreply.github.com>
2023-03-12 21:50:44 +0000

    ✨ Add sessions list & manage state (#3)
    * create timer view
    
    * create counter button
    
    * add nav view
    
    * create sessions list
    
    * properly save state and reset timer

--- Kickr/Shared/ContentView.swift
+++ Kickr/Shared/ContentView.swift
@@ -7,7 +7,18 @@
 
 import SwiftUI
 
+struct Session: Identifiable, Equatable {
+    let id = UUID()
+    var count: Int
+    var duration: Double
+    var isActive: Bool
+    let date: Date
+}
+
 struct ContentView: View {
+    @State var sessions: [Session] = []
+    @State var currentSession: Session = Session(count: 0, duration: 0, isActive: false, date: Date())
+        
     var body: some View {
         NavigationView {
         ZStack {
@@ -16,9 +27,20 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .edgesIgnoringSafeArea(.all)
             
-            VStack(alignment: .center, spacing: 16) {
-                TimerView()
-                KickCountView()
+            VStack(alignment: .center, spacing: 30) {
+                TimerView(currentSession: self.$currentSession)
+                KickCountView(currentSession: self.$currentSession)
+                Button {
+                    sessions.append(currentSession)
+                    currentSession = Session(count: 0, duration: 0, isActive: false, date: Date())
+                    
+                                    } label: {
+                    Text("Save Session")
+                }
+                
+                ForEach(sessions) { session in
+                    Text("\(session.count) kicks, \(session.duration.hourMinuteSecondMS)")
+                }
             }
         }
         .navigationTitle("Kickr")
--- Kickr/Shared/Timer/TimerView.swift
+++ Kickr/Shared/Timer/TimerView.swift
@@ -8,16 +8,24 @@
 import SwiftUI
 
 struct TimerView: View {
-    @State var isRunning = false
+    @State private var isRunning = false
     @State private var timerString = "0.00"
     @State private var start = Date()
     @State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
+    @Binding var currentSession: Session
+    
     var body: some View {
         Text(self.timerString)
+            .onChange(of: currentSession) { session in
+                if (!session.isActive) {
+                    self.stopTimer()
+                }
+            }
             .font(Font.system(.largeTitle, design: .monospaced))
             .onReceive(timer) { _ in
                 if self.isRunning {
+                    currentSession.duration = Date().timeIntervalSince(self.start)
                     timerString = String(format: "%.2f", (Date().timeIntervalSince(self.start)))
                 }
             }
@@ -31,16 +39,20 @@ struct TimerView: View {
                     self.startTimer()
                 }
                 isRunning.toggle()
-            }.onAppear() {
-                self.stopTimer()
             }
     }
     
     func stopTimer() {
+        timerString = "0.00"
         self.timer.upstream.connect().cancel()
     }
     
     func startTimer() {
+        currentSession.isActive = true
         self.timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    }
+    
+    func resetTimer() {
+        
     }
 }
